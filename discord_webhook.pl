3cf800e0225e5990f4ae9649fb6bde827868afc1
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-10 22:42:49 +0900

    webhookの実装

---
+++ discord_webhook.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use HTTP::Tiny;
+use JSON::PP;
+
+my $webhook_url = 'https://discord.com/api/webhooks/1062339671415341116/868ZyYHu1HlmThosNcIh7QKCwopSoBvs4Sz65N-d_lLcEO86dOESq9LoDfmLwvFJZIW_';
+
+my $options = {
+    headers => { 'Content-Type' => 'application/json'},
+    content => JSON::PP->new()->encode({ content => 'Hello from Webhook' }),
+};
+
+my $response = HTTP::Tiny->new()->post( $webhook_url, $options );
+print Dumper $response;
